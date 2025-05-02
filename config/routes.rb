08706a195b5892032892250fb0529ff8e3632def
mikaelaguerrero <miguerrero@miuandes.cl>
2025-05-02 01:11:45 -0400

    Lab 6 volvio a lo de antes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :messages, only: [:index, :show]
-  resources :chats, only: [:index, :show]
   resources :users, only: [:index, :show]
+  resources :chats, only: [:index, :show]
+  resources :messages, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
