2c723f6c0c2151a794eb5c9eeae71852021a0e7c
mikaelaguerrero <miguerrero@miuandes.cl>
2025-05-16 17:28:09 -0400

    lab 8 listoo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :chats
-  resources :messages
+
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :chats, only: [:index, :show, :new, :create, :edit, :update]
+  resources :messages, only: [:index, :show, :new, :create, :edit, :update]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
